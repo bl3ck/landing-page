d2706bddf9971792ec80ce2bfce261a492534926
Eugen-Bleck <eugenbleck@gmail.com>
2020-05-31 00:47:42 +0100

    fix: active sections

--- js/app.js
+++ js/app.js
@@ -38,9 +38,7 @@ window.addEventListener('load', function () {
     console.log('Section with Nav', navSections);
 
     function scrollTo(target) {
-        $('html,body').animate({
-            scrollTop: target ? target.offset().top : 0
-        }, 'slow');
+        target.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
     }
 
     // Get all section ID's and Names for menu
@@ -70,17 +68,17 @@ window.addEventListener('load', function () {
 
         sectionsObject.forEach((section) => {
             const nav_item = document.createElement('li');
-            nav_item.innerHTML = `<a class="menu__link" href="#${section.hash}">${section.name}</a>`;
+            nav_item.innerHTML = `<a class="menu__link" data-id="${section.hash}" href="#${section.hash}">${section.name}</a>`;
             nav_menu_lis.push(nav_item);
             document.getElementById("navbar__list").appendChild(nav_item);
-            nav_item.onclick = () => scrollTo($(`#${section.hash}`));
-        });
+            nav_item.onclick = (e) => {
+                e.preventDefault();
+                const elementToScrollTo = document.getElementById(`${section.hash}`)
+                elementToScrollTo.scrollIntoView({ block: 'center', behavior: 'smooth' })
 
-        // sectionsObject.forEach((section) => {
-        //     let li_node = document.createElement("li");
-        //     let data = 'fish';
-        //     nav_menu_lis.push(li_node);
-        // });
+                // scrollTo((`#${section.hash}`));
+            };
+        });
 
         return nav_menu_lis;
     }
@@ -101,21 +99,34 @@ window.addEventListener('load', function () {
 
     // Scroll to anchor ID using scrollTO event
     // root is the browser viewport / screen
-    var observer = new IntersectionObserver(function (entries) {
+    var observer = new IntersectionObserver((entries) => {
         // since there is a single target to be observed, there will be only one entry
         let section = entries[0].target;
+        // console.log('Section list', section);
+
+        const navListElement = document.querySelector(`.menu__link[data-id='${section.id}']`);
+        const section_h = document.getElementById(section.hash)
+
+        // get_links
         if (entries[0]['isIntersecting'] === true) {
-            if (entries[0]['intersectionRatio'] > 0.2) {
+
+            if (entries[0]['intersectionRatio'] > 0.8) {
+                console.log('Intersecting');
                 // let current_section = document.getElementById(`${section.hash}`);
                 section.classList.toggle('your-active-class');
-                console.log('class list', section.classList);
+                // console.log('class list', section.classList);
+                navListElement.classList.add('active__link')
+            }
+            else if (navListElement.classList.contains('active__link')) {
+                navListElement.classList.remove('active__link')
             }
         }
         else {
-            // section.classList.toggle('your-active-class');
-            console.log('D class list', section.classList);
+            section.classList.toggle('your-active-class');
+            console.log('Not Intersecting');
         }
-    }, { threshold: [0, 0.3] });
+
+    }, { threshold: [0, 0.8] });
 
     sectionsObject.forEach(section => {
         observer.observe(document.querySelector(`#${section.hash}`));
